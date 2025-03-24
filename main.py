2b133869f11cd8a1b210037bb07a4633094c2353
Gypra <gytisnaujasis@icloud.com>
2025-03-24 11:37:17 +0200

    working on sol

--- main.py
+++ main.py
@@ -1,40 +1,63 @@
 # Is different solution cause bishops can go in the same row, so updates are needed
-
+from copy import copy, deepcopy
 
 def solveNBishops(n):
-    queen   = "R"
-    results, stack = [], []
-    board_row = ["." for i in range(n)]
+    bishop = "B"
+    results = []
+    board = [["." for _ in range(n)] for _ in range(n)]
     diagsPos, diagsNeg = set(), set()
     
-    """
-         For neighbor:
-        #################
-         dP = row + col
-         dN = col - row
-        ###############3
-    """
-    def backtracking(row):
-        if row == n:                                                                # Row out of bounds, reached limit
-            str_stack = ["".join(col) for col in stack]                             # Turn into strings
-            results.append(str_stack[:])                                            # Append string lists to result list 
+    def backtracking(row, col, curr_board=[]):
+        bish_cnt = len(diagsNeg) # Or positive, doesnt matter
+        if bish_cnt >= n:
+            results.append(deepcopy(curr_board))
+            curr_board = board[:]
+            return
+
+        if row >= n or col >= n:
             return
 
         for col in range(n):
             # Check if doesn't violate constraints
             if (row - col) in diagsNeg or (row + col) in diagsPos:
                 continue
+
+            # Add bishop to board
+            curr_board[row][col] = bishop
+
+            # Add diagonal constraints
+            diagsPos.add(row + col); diagsNeg.add(row - col)
+            backtracking(row, col + 1, curr_board)
+
+            # Remove last bishop
+            curr_board[row][col] = "."
+            diagsPos.remove(row + col); diagsNeg.remove(row-col)
         
-            diagsPos.add(row + col); diagsNeg.add(row - col)                        # Add to set of vals to enforce constrains 
+        for row in range(n):
+            # Check if doesn't violate constraints
+            if (row - col) in diagsNeg or (row + col) in diagsPos:
+                continue
+            
+            # Add bishop to board
+            curr_board[row][col] = bishop
 
-            board_current_col = board_row[:]                                        # Copy board row
-            board_current_col[col] = queen
-            stack.append(board_current_col)                                         # Found solution, go next row
+            diagsPos.add(row + col); diagsNeg.add(row - col)
+            backtracking(row + 1, col, curr_board)
 
-            backtracking(row + 1)                                                   # Go next row tree
-            stack.pop()                                                             # Remove solution
-            diagsPos.remove(row + col); diagsNeg.remove(row-col)                    # Remove after backtracking from solution to try different ones
+            curr_board[row][col] = "."
+            diagsPos.remove(row + col); diagsNeg.remove(row-col)
 
-
-    backtracking(0)
+    cp = deepcopy(board)
+    backtracking(0, 0, cp)
     return results
+
+for _ in solveNBishops(2):
+    print(_)
+
+"""
+    2x2 for 2 bishops solutions: 4
+     +                       +       + 
+    B B         B .         . .     . B
+    . .         B .         B B     . B 
+
+"""
